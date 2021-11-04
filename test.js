c9daf589ccbca9562bb2facf8de8d4ff430fa11d
zekeg <zechariah.guadiana@gmail.com>
2021-11-04 18:38:32 -0500

    Initial Upload of Puppeteer Scraper
    Scrapes designated website with Puppeteer package in NodeJS

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 //Hello! I wanted to create this to demonstrate some basic competencies with Puppeteer, JS and Node
 //This will scrape the provided website and then use the data it collects to create a reduced HTML document
+
 //Get Puppeteer, initiate readline for asking the user questions
 const puppeteer = require('puppeteer');
 fs = require('fs');
@@ -11,7 +12,7 @@ function askQuestion(query) {
     input: process.stdin,
     output: process.stdout,
   });
-//This returns a promise so we can query the user then use await in the asynchronus statement
+  //This returns a promise so we can query the user then use await in the asynchronus statement
   return new Promise(resolve => rl.question(query, ans => {
     rl.close();
     resolve(ans);
@@ -20,13 +21,14 @@ function askQuestion(query) {
 
 let appObjs = [];
 (async () => {
-  const answer = await askQuestion("Press return to continue with scraping data from https://www.q2developer.com/marketplace")
-  
+  //A quick prompt to continue
+  const answer = await askQuestion("Press return to continue with scraping data from https://www.q2developer.com/marketplace");
+  //ask for our Puppeteer functionality
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  //use our new browser to go to the website
   await page.goto("https://www.q2developer.com/marketplace");
-
-  await page.screenshot({ path: 'example.png' });
+  //use $$eval to select relevant app links and collect data about them
   const titles = await page.$$eval('.app-link', appLinks => appLinks.map(appLink => {
     return {
       link: appLink.href,
@@ -35,10 +37,16 @@ let appObjs = [];
       logoImageAddress: appLink.querySelector('.avatar-img').src
     }
   }))
+  //close the browser, store the data in a variable and show the user
   await browser.close();
   appObjs = titles;
   console.log("app objects", appObjs);
-  fs.writeFileSync("names.html", appObjs.map(obj => `<div class="app-box"><a href="${obj.link}"><img src="${obj.logoImageAddress}" style="height: 100px;"/><br><h1>${obj.title}</h1></a></div>`).join(' '));
+  //create an HTML document with the HTML app data we've collected
+  fs.writeFileSync("names.html", appObjs
+    .map(obj => `<div class="app-box"><a href="${obj.link}"><img src="${obj.logoImageAddress}" style="height: 100px;"/><br><h1>${obj.title}</h1></a></div>`).join(' '));
+    
+  console.log("an HTML file has been created in the current directory with all valid apps and their links");  
+  //disconnect and exit the Node program
   browser.on('disconnected', process.exit(1));
 }
 )();
